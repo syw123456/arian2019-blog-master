08c756dbb248adf25958fe46eda6df423537135b
yuewei shi <2671523019@qq.com>
2019-05-01 16:26:14 +0800

    commit

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="content">
-
     <h1 class="name">About</h1>
-
     <p>I am a <strong>digital creator</strong>, located at the intersection between full-stack development, user
       experience, and interface design. I am the <strong>editor of javascript in plain english</strong>. i am currently
       working as <strong>a full-stack developer</strong> for a behavioural psychology-led research and design agency in
       China.</p>
-
     <nuxt-link to="/media" class="button">media</nuxt-link>
-
   </div>
 </template>
-
 <script>
-
-
   export default {
     components: {}
   }
@@ -31,8 +24,6 @@
     word-spacing: -0.04em;
     line-height: 1.3em;
   }
-
-
   @media only screen and (max-width: 599px) {
     h2, h3, h4, h5, p {
       font-size: 1.2em;
@@ -42,7 +33,5 @@
       word-spacing: -0.04em;
       line-height: 1.4em;
     }
-
   }
-
 </style>
