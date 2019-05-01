beec903ec308c0165d76e3a2ff35d90ede0ef019
yuewei shi <2671523019@qq.com>
2019-05-01 18:35:31 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,9 @@
     <div class="content">
 
       <h1 class="name">Arian</h1>
-      <p style="font-weight:500; font-size:2.2em">My name is Arian.</p>
+
       <h2 class="tagline">developer + <br/>app designer </h2>
+
       <nuxt-link to="/about" class="button">learn more</nuxt-link>
 
   </div>
