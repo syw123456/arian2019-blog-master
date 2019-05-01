bfa8b965fe9b0b774f2f09f153ef8dcc69bdc501
yuewei shi <2671523019@qq.com>
2019-05-01 11:36:32 +0800

    commit

--- pages/about.vue
+++ pages/about.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="content">
+  <div class="content">
 
-        <h1 class="name">About</h1>
+    <h1 class="name">About</h1>
 
-        <p>I am a <strong>digital creator</strong>, located at the intersection between full-stack development, user experience, and interface design. i am the <strong>editor of javascript in plain english</strong>. i am currently working as <strong>a full-stack developer</strong> for a behavioural psychology-led research and design agency in the Makati.</p>
+    <p>I am a <strong>digital creator</strong>, located at the intersection between full-stack development, user
+      experience, and interface design. I am the <strong>editor of javascript in plain english</strong>. i am currently
+      working as <strong>a full-stack developer</strong> for a behavioural psychology-led research and design agency in
+      Makati.</p>
 
-        <nuxt-link to="/media" class="button">media</nuxt-link>
+    <nuxt-link to="/media" class="button">media</nuxt-link>
 
-    </div>
+  </div>
 </template>
 
 <script>
 
 
-    export default {
-        components: {
-        }
-    }
+  export default {
+    components: {}
+  }
 </script>
 
 <style scoped>
@@ -32,14 +34,14 @@
 
 
   @media only screen and (max-width: 599px) {
-      h2, h3, h4, h5, p {
-          font-size: 1.2em;
-          margin: 5px 0;
-          text-align: left;
-          color: #646464;
-          word-spacing: -0.04em;
-          line-height: 1.4em;
-      }
+    h2, h3, h4, h5, p {
+      font-size: 1.2em;
+      margin: 5px 0;
+      text-align: left;
+      color: #646464;
+      word-spacing: -0.04em;
+      line-height: 1.4em;
+    }
 
   }
 
