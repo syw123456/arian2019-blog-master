d19c17eb00393174b9b0d6acb5b716c477b2c5da
yuewei shi <2671523019@qq.com>
2019-05-03 22:31:29 +0800

    update myself

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="content">
-      <h1 class="name">Hello.</h1>
+      <h1 class="name" data-text="Hello">Hello.</h1>
       <h2> My name is Arian.</h2>
       <h2 class="tagline">developer + <br/>app designer </h2>
       <nuxt-link to="/about" class="button">learn more</nuxt-link>
